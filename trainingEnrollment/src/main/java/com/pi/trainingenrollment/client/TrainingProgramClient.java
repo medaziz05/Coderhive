97b2b570735ab4f9973a7fae62ce522dcb8ae82f
wissaldaoud <daoudwissal2000@gmail.com>
2025-04-13 01:32:59 +0200

    ParticipantController modified

--- trainingEnrollment/src/main/java/com/pi/trainingenrollment/client/TrainingProgramClient.java
+++ trainingEnrollment/src/main/java/com/pi/trainingenrollment/client/TrainingProgramClient.java
@@ -1,4 +1,22 @@
+// src/main/java/com/pi/trainingenrollment/client/TrainingProgramClient.java
+
 package com.pi.trainingenrollment.client;
 
+import com.pi.trainingenrollment.model.TrainingProgram;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class TrainingProgramClient {
+
+    private final RestTemplate restTemplate;
+
+    public TrainingProgramClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public TrainingProgram getTrainingProgramById(int id) {
+        String url = "http://trainingprogram-service/api/v1/formation/" + id;
+        return restTemplate.getForObject(url, TrainingProgram.class);
+    }
 }
